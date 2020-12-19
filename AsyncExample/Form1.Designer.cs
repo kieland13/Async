069e389df947906944bd3a1389a1ddaece1fb95c
Nick Kieland <nkieland@icloud.com>
2020-12-18 19:21:20 -0600

    First commit

---
+++ AsyncExample/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace AsyncExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMath = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMath
+            // 
+            this.txtMath.Location = new System.Drawing.Point(127, 77);
+            this.txtMath.Name = "txtMath";
+            this.txtMath.Size = new System.Drawing.Size(100, 20);
+            this.txtMath.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(351, 77);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(383, 637);
+            this.txtResult.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(127, 224);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButtton_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(537, 29);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 726);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtMath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMath;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ AsyncExample/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButtton_Click(object sender, EventArgs e)
+        {
+            int x = 5;
+            DisplayWebResult();
+            double y = Math.Pow(x, 20.0);
+            txtMath.Text = y.ToString();
+        }
+
+        private async void DisplayWebResult()
+        {
+            lblStatus.Text = "Fetching......";
+            using (HttpClient client = new HttpClient() )
+            {
+                Task<string> task = client.GetStringAsync("http://www.dctc.edu");
+                string text = await task;
+                txtResult.Text = text;
+                lblStatus.Text = "Fetched......";
+            }
+        }
+    }
+}
